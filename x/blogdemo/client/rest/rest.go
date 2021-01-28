3870034a1ff80772d36e47c93d123c821b0825c6
Jacob Gadikian <jacobgadikian@gmail.com>
2021-01-28 10:37:22 +0700

    Added blog features

--- x/blogdemo/client/rest/rest.go
+++ x/blogdemo/client/rest/rest.go
@@ -14,12 +14,22 @@ const (
 // RegisterRoutes registers blogdemo-related REST handlers to a router
 func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 2
+	registerQueryRoutes(clientCtx, r)
+	registerTxHandlers(clientCtx, r)
+
 }
 
 func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 3
+	r.HandleFunc("/blogdemo/posts/{id}", getPostHandler(clientCtx)).Methods("GET")
+	r.HandleFunc("/blogdemo/posts", listPostHandler(clientCtx)).Methods("GET")
+
 }
 
 func registerTxHandlers(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 4
+	r.HandleFunc("/blogdemo/posts", createPostHandler(clientCtx)).Methods("POST")
+	r.HandleFunc("/blogdemo/posts/{id}", updatePostHandler(clientCtx)).Methods("POST")
+	r.HandleFunc("/blogdemo/posts/{id}", deletePostHandler(clientCtx)).Methods("POST")
+
 }
